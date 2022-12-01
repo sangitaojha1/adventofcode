09fb9d208a25cf182135e3a2e818520c3c90ac5b
sojha <sojha@cgs-online.de>
2022-12-01 11:20:54 +0100

    2022 - Day1 code formatted

--- Day1.js
+++ Day1.js
@@ -1,51 +1,47 @@
-var data = document.body.children[0].textContent.split("\n").map((x) => (x));;
+data = document.body.children[0].textContent.split("\n").map((x) => (x));;
 console.log(data);
 temp = 0;
-var tempWitSum = [];
+tempWitSum = [];
 for (k = 0; k < data.length; k++) {
-    if (data[k] == "") {
-        tempWitSum.push(temp);
-        temp = 0;
-    } else {
-        temp = temp + parseInt(data[k]);
-    }
+  if (data[k] == "") {
+    tempWitSum.push(temp);
+    temp = 0;
+  } else {
+    temp = temp + parseInt(data[k]);
+  }
 
 }
 console.log(tempWitSum);
 max = tempWitSum[0];
 for (i = 1; i < tempWitSum.length; i++) {
-    if (tempWitSum[i] > max) {
-        max = tempWitSum[i];
-    }
+  if (tempWitSum[i] > max) {
+    max = tempWitSum[i];
+  }
 }
 console.log(max);
 
-var maxIndex = new Array();
-var maxCalories = new Array();
+maxIndex = new Array();
+maxCalories = [0, 0, 0];
 
-maxCalories[0] = 0;
-maxCalories[1] = 0;
-maxCalories[2] = 0;
 
 for (i = 0; i < tempWitSum.length; i++) {
-    if (tempWitSum[i] > maxCalories[0]) {
-        maxCalories[0] = tempWitSum[i];
-        maxIndex[0] = i;
-    }
+  if (tempWitSum[i] > maxCalories[0]) {
+    maxCalories[0] = tempWitSum[i];
+    maxIndex[0] = i;
+  }
 }
 
 for (i = 0; i < tempWitSum.length; i++) {
-    if (tempWitSum[i] > maxCalories[1] && tempWitSum[i] < maxCalories[0]) {
-        maxCalories[1] = tempWitSum[i];
-        maxIndex[1] = i;
-    }
+  if (tempWitSum[i] > maxCalories[1] && tempWitSum[i] < maxCalories[0]) {
+    maxCalories[1] = tempWitSum[i];
+    maxIndex[1] = i;
+  }
 }
 
 for (i = 0; i < tempWitSum.length; i++) {
-    if (tempWitSum[i] > maxCalories[2] && tempWitSum[i] < maxCalories[1]) {
-        maxCalories[2] = tempWitSum[i];
-        maxIndex[2] = i;
-    }
+  if (tempWitSum[i] > maxCalories[2] && tempWitSum[i] < maxCalories[1]) {
+    maxCalories[2] = tempWitSum[i];
+    maxIndex[2] = i;
+  }
 }
 console.log(maxCalories[0] + maxCalories[1] + maxCalories[2]);
-
